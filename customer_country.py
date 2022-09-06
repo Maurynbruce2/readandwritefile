069eb0da209597275be77648f57fdbf54ca9b5d7
Mauryn <Mauryn_bruce2@baylor.edu>
2022-09-06 13:03:40 -0500

    Add Customer Country

--- customer_country.py
+++ customer_country.py
@@ -3,20 +3,25 @@ def main():
 
     import csv
     infile = open('customers.csv','r')
+    outfile = open('customer_country.csv','w')
 
     csvfile = csv.reader(infile, delimiter=',')
 
-    Header = 'Full Name' , 'Country'
-    Index = [0,4]
-    Data = Index
+    next(csvfile)
 
-    customer_country = open('path/to/csv_file','w')
-    writer = csv.writer(customer_country)
+    outfile.write('Full Name, Country\n')
 
-    writer.writerow(Header)
-    csvfile.close()
+    for record in csvfile: 
+        full_name = record[1] + ' ' + record[2]
+        country = record[4]
+
+        outfile.write(full_name + ',' + country + '\n')
+
+    outfile.close()
+
 
 main()
+
 
 
 
